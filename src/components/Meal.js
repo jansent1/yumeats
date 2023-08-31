c50cb6bf434c7568b2f65ebc686feeab49d237ed
Teun Jansen <teunjansen91@gmail.com>
2023-08-31 11:20:11 +0200

    Meal section V.1.0

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,4 +1,5 @@
 import { mealData } from "../data/data";
+import { ArrowSmRightIcon } from "@heroicons/react/outline";
 
 const Meal = () => {
   return (
@@ -6,14 +7,31 @@ const Meal = () => {
       <h1 className="text-orange-500 font-bold text-2xl text-center py-2">
         Our Meals
       </h1>
-      <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6">
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-6 py-4">
         {mealData.map((item) => (
-          <div className="border-none hover:scale-105 duration-300">
+          <div
+            key={item.id}
+            className="border-none hover:scale-105 duration-300"
+          >
             <img
               className="w-full h-[200px] object-cover rounded-lg"
               src={item.image}
               alt={item.name}
             />
+            <div className="flex justify-between py-2 px-2">
+              <p className="font-bold">{item.name}</p>
+              <p className="bg-orange-700 h-18 w-18 rounded-full -mt-10 text-white py-4 px-2 border-8 font-bold">
+                {item.price}
+              </p>
+            </div>
+            <div className="">
+              <div className="pl-2 py-4 -mt-7">
+                <p className="flex items-center text-indigo-600">
+                  View More
+                  <ArrowSmRightIcon className="w-5 ml-2" />
+                </p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
